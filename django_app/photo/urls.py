e16c834cb574d99be18aae9c806e12efbf226d8b
Pando <ndjman7@gmail.com>
2016-10-25 19:44:31 +0900

    Make album_list init (urls.py, album_list.html, views/album.py-album_list)

---
+++ django_app/photo/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    url(r'^album/$', views.album_list, name='album_list'),
+]
--- django_app/photo/views/album.py
+++ django_app/photo/views/album.py
@@ -1,3 +1,10 @@
+from django.shortcuts import render
+from photo.models import Album
 __all__ = [
-
+    'album_list',
 ]
+
+
+def album_list(request):
+    albums = Album.objects.all()
+    return render(request, 'photo/album_list.html', {'albums': albums})
