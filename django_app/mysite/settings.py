37b5ed0207c3f86a279d4c09199dde9381ae833d
Pando <ndjman7@gmail.com>
2016-10-25 18:00:48 +0900

    .gitignore add Media/ settings.py add MEDIA_ROOT & MEDIA_ROOT / urls.py add static Media Folder

--- django_app/mysite/settings.py
+++ django_app/mysite/settings.py
@@ -28,6 +28,10 @@ STATICFILES_DIRS = [
     STATIC_DIR,
 ]
 
+# Media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 AUTH_USER_MODEL = 'member.MyUser'
 # AUTH_USER_MODEL = 'django.contrib.auth.models.User'
 AUTHENTICATION_BACKENDS = [
