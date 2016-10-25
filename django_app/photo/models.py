596f09a72fc64c8476a0e0fd7e966db12fd0b0c9
Pando <ndjman7@gmail.com>
2016-10-25 16:32:04 +0900

    Make Model Photo, Album

--- django_app/photo/models.py
+++ django_app/photo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
+"""
+Album
 
-# Create your models here.
+Photo
+"""
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=30)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    description = models.CharField(max_length=80, blank=True)
+
+
+class Photo(models.Model):
+    album = models.ForeignKey(Album)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=80, blank=True)
+    img = models.ImageField(upload_to='photo')
