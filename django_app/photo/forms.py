f06d127af945fd64aa83e2f27923dd50d5962a6c
Pando <ndjman7@gmail.com>
2016-10-26 20:25:14 +0900

    Make Add photo

--- django_app/photo/forms.py
+++ django_app/photo/forms.py
@@ -11,3 +11,21 @@ class AlbumModelForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.TextInput(attrs={'class': 'form-control'}),
         }
+
+
+class PhotoForm(forms.Form):
+    title = forms.CharField(
+        max_length=30,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control',}
+        )
+    )
+    description = forms.CharField(
+        max_length=80,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control',}
+        )
+    )
+    img = forms.ImageField(
+        widget=forms.FileInput()
+    )
--- django_app/photo/views/photo.py
+++ django_app/photo/views/photo.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from photo.forms import PhotoForm
+from photo.models import Album
 __all__ = [
     'photo_new',
 ]
 
 
-def photo_new(request):
-    return render(request, 'photo/photo_edit.html', {})
+def photo_new(request, album_pk):
+    context = {}
+    user = request.user
+    if request.method == 'POST':
+        form = PhotoForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            description = form.cleaned_data['description']
+            img = form.cleaned_data['img']
+
+            album = Album.objects.get(pk=album_pk)
+            album.photo_set.create(
+                album=album,
+                owner=user,
+                title=title,
+                description=description,
+                img=img,
+            )
+        return redirect('photo:album_detail', pk=album_pk)
+    else:
+        form = PhotoForm()
+        context['form'] = form
+    return render(request, 'photo/photo_edit.html', context)
